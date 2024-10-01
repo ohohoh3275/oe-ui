cd52a0a52a0d2fb664b6e4e976ecfe22fc69cd41
lee-gyu <leegyu4dev@gmail.com>
2024-10-01 22:25:14 +0900

    ✨ feat(component): 스타일링 기반 추가

--- packages/components/src/button.tsx
+++ packages/components/src/button.tsx
@@ -1,8 +1,10 @@
+import css from "./_button.module.css";
+
 type ButtonProps = {
     text?: string;
 };
 
 export function Button(props: ButtonProps) {
     const { text } = props;
-    return <button>{text}</button>;
+    return <button className={css.button}>{text}</button>;
 }
---
+++ packages/components/types/global.d.ts
@@ -0,0 +1,5 @@
+// css module declaration
+declare module "*.module.css" {
+    const classes: { [key: string]: string };
+    export default classes;
+}
