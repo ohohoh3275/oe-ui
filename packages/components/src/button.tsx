0cd2e6e595597dbb269bfa9c95c3bdcea5d63d2a
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-10-22 16:30:39 +0900

    ✨ feat(button): loader 컴포넌트 함수 외부로 정의

--- packages/components/src/button.tsx
+++ packages/components/src/button.tsx
@@ -1,12 +1,44 @@
 import css from "./_button.module.css";
 
+/*
+- properties
+    text: string
+    isLoading: boolean
+    size: sm, md, lg, xl
+    variant: default, primary, success, warning, error, link
+*/
+
+type Size = "sm" | "md" | "lg" | "xl";
+type Variant = "default" | "primary" | "success" | "warning" | "error" | "link";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     text?: string;
-    size?: "xs" | "small" | "medium" | "large" | "xl";
-    variant?: "default" | "success" | "warning" | "error";
+    size?: Size;
+    variant?: Variant;
+    isLoading?: boolean;
 }
 
+const Loader = () => {
+    return (
+        <div className={css.loader}>
+            <div className={css.dot} />
+            <div className={css.dot} />
+            <div className={css.dot} />
+        </div>
+    );
+};
+
 export function Button(props: ButtonProps) {
-    const { text } = props;
-    return <button className={css.button}>{text}</button>;
+    const { text, size = "md", variant = "default", isLoading = false } = props;
+
+    return (
+        <button
+            className={`${css.button__wrapper} ${
+                css[`button__wrapper--${size}`]
+            } ${css[`button__wrapper--${variant}`]}`}
+            disabled={isLoading}
+        >
+            {isLoading ? <Loader /> : text}
+        </button>
+    );
 }
