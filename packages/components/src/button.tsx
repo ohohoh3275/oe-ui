9ff56c3e02d0a264c5724826f5b75c76422e536c
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2024-10-29 17:03:20 +0900

    ✨ feat(button): 버튼 텍스트 감싸는 태그 추가

--- packages/components/src/button.tsx
+++ packages/components/src/button.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import css from "./_button.module.css";
-
 /*
 - properties
     text: string
@@ -29,7 +29,13 @@ const Loader = () => {
 };
 
 export function Button(props: ButtonProps) {
-    const { text, size = "md", variant = "default", isLoading = false } = props;
+    const {
+        text,
+        size = "md",
+        variant = "default",
+        isLoading = false,
+        ...rest
+    } = props;
 
     return (
         <button
@@ -37,8 +43,11 @@ export function Button(props: ButtonProps) {
                 css[`button__wrapper--${size}`]
             } ${css[`button__wrapper--${variant}`]}`}
             disabled={isLoading}
+            {...rest}
         >
-            {isLoading ? <Loader /> : text}
+            <span className={css.button__content}>
+                {isLoading ? <Loader /> : text}
+            </span>
         </button>
     );
 }
